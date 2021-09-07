94619d5b395e2c5f3bfa1dba84fd59cbcc146588
Wookiee- <taggart_james@hotmail.com>
2021-09-07 06:55:55 +0000

    install.sh update again

--- install.sh
+++ install.sh
@@ -90,11 +90,12 @@ while [ $opt != '' ]
         ;;
         3) clear;
             option_picked "\n${menu} Installing MBIIWeb Tools...${normal}\n";
-                apt-get update
 		wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 		dpkg -i packages-microsoft-prod.deb
 		rm packages-microsoft-prod.deb
 
+
+                apt-get update
 		apt-get install -y apt-transport-https
 		apt-get install -y dotnet-sdk-5.0
 		apt-get install -y dotnet-sdk-3.1
